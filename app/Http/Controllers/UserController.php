716cf10e1fa992c01be8621540f8ab33bd51d1fd
eric mwangi <nuthuerick147@gmail.com>
2024-04-18 17:24:29 +0300

    added files and register form,delete and edit function

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class UserController extends Controller
+{
+    //Show Register/Create Form
+    public function create(){
+        return view('users.register');
+    }
+
+    //create New User
+    public function store(Request $request){
+        $formFields = $request->validate([
+            'name' => ['required', 'min:3'],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required, confirmed, min:6']
+        ]);
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\ListingController;
+use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Listing;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,5 +34,21 @@ Route::get('/listings/create',[ListingController::class, 'create']);
 ///store Listing Data
 Route::post('/listings', [ListingController::class, 'store']);
 
+//Show Edit Form
+Route::get('/listings/{listing}/edit',[ListingController::class, 'edit']);
+
+//Update Listing
+Route::put('/listings/{listing}', [ListingController::class, 'update'] );
+
+//Delete Listing
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy'] );
+
+
 ///single listings
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+//Show Register/Create form
+Route::get('/register', [UserController::class, 'create']);
+
+//Create new user
+Route::post('/users', [UserController::class, 'store']);
