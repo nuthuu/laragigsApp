127554725c67c6151e94f999dac537ea1ede7595
eric mwangi <nuthuerick147@gmail.com>
2024-05-21 16:04:22 +0300

    changed tag to tags

--- resources/views/components/listing-tags.blade.php
+++ resources/views/components/listing-tags.blade.php
@@ -10,7 +10,7 @@
     <li
         class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
     >
-        <a href="/?tag={{ $tag }}">{{ $tag }}</a>
+        <a href="/?tags={{ $tags }}">{{ $tags }}</a>
     </li>
     @endforeach
 </ul>
